df0c3b7899118bb349dd94e740c1f5b6a8c01cad
amirspahiu77 <spahiuamir10@gmail.com>
2025-03-06 19:14:52 +0100

    Add files via upload

---
+++ DS_Theme/index.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+<body>
+    <!---->
+    <div id="content" class="site-content">
+        <div class="content-area" id="primary">
+            <main id="main" class="site-main">
+                <section class="hero">
+                    Hero
+                </section>
+                <section class="services">
+                    Services
+                </section>
+                <section class="home-blog">
+                    Blog
+                </section>
+            </main>
+        </div>
+    </div>
+</body>
+</html>
+<?php get_footer(); ?>
