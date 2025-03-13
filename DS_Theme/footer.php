29908126e48f1b166ae433f8998dc6e8e4128269
amirspahiu77 <spahiuamir10@gmail.com>
2025-03-13 19:14:40 +0100

    Add files via upload

--- DS_Theme/footer.php
+++ DS_Theme/footer.php
@@ -1,5 +1,13 @@
 <footer class="site-footer">
-        Footer
-    </footer>
+    <div class="container">
+         <div class="copyright">
+            <p>Copyright DS Theme - All rights reserved</p>
+         </div>
+         <nav class="footer-menu">
+            <?php wp_nav_menu(array('theme_location' => 'wp_devs_footer_menu', 'depth' => 1 )); ?>
+         </nav>
+    </div>
+</footer>
+<?php wp_footer(); ?>
 </body>
 </html>
--- DS_Theme/header.php
+++ DS_Theme/header.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
-<html <?php language_attributes() ?>>
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset')?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
-<body>
-    <?php body_class(); ?>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, similique.</p>
+<body <?php body_class(); ?>>
     <header>
         <section class="top-bar">
             <div class="logo">
@@ -20,7 +18,16 @@
         </section>
         <section class="menu-area">
             <nav class="main-menu">
-                Menu
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'wp_devs_main_menu',
+                        'depth' => 2
+                    )
+                );
+                ?>
             </nav>
         </section>
     </header>
+</body>
+</html>
